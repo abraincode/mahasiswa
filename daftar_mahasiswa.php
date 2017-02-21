71cdd44ddcc6d9288780cacca891537e95707a62
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-22 02:18:29 +0700

    tambah table dgn data table

--- daftar_mahasiswa.php
+++ daftar_mahasiswa.php
@@ -10,6 +10,9 @@
 </head>
 
 <body>
+<div class="container">
+
+</div>
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -73,48 +76,68 @@
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
-
 <?php
 require_once 'class_mahasiswa.php'; //untuk menyertakan atau memasukan file/library untuk di gunakan
 
-$mhs1 = new Mahasiswa ("02011", "Faiz Fikri");
+$mhs1 = new Mahasiswa ("1", "02011", "Faiz Fikri");
 $mhs1->prodi = "TI";
 $mhs1->thn_angkatan=2012;
 $mhs1->ipk=3.8;
-echo "Nama :" .$mhs1->nama;
-echo "<br/>Nim :" .$mhs1->nim;
-echo "<br/>IPK :" .$mhs1->ipk;
-echo "<br/>Predikat :" .$mhs1->predikat_ipk();
 
-$mhs2 = new Mahasiswa ("02012", "Alissa Khairunnisa");
+$mhs2 = new Mahasiswa ("2", "02012", "Alissa Khairunnisa");
 $mhs2->prodi = "TI";
 $mhs2->thn_angkatan=2012;
 $mhs2->ipk=3.9;
 
-$mhs3 = new Mahasiswa ("01011", "Rosalie Naurah");
+$mhs3 = new Mahasiswa ("3", "01011", "Rosalie Naurah");
 $mhs3->prodi = "SI";
 $mhs3->thn_angkatan=2010;
 $mhs3->ipk=3.46;
 
-$mhs4 = new Mahasiswa ("01012", "Defgi Muhammad");
+$mhs4 = new Mahasiswa ("4", "01012", "Defgi Muhammad");
 $mhs4->prodi = "SI";
 $mhs4->thn_angkatan=2010;
 $mhs4->ipk=3.2;
-
-// buat array
-echo "<hr>";
-$ar_mahasiswa = [$mhs1,$mhs2,$mhs3,$mhs4];
-foreach($ar_mahasiswa as $mhs){
-    echo $mhs->nim . ' - ' .
-         $mhs->nama .' - ' .
-         $mhs->thn_angkatan .' - ' .
-         $mhs->prodi .' - ' .
-         $mhs->predikat_ipk();
-    echo "<br/>";
-    ;
-}
-
-
 ?>
+
+      <table id="table_id" class="display">
+          <thead>
+              <tr>
+                  <th>No. </th>
+                  <th>NIM</th>
+                  <th>Nama</th>
+                  <th>Prodi</th>
+                  <th>Thn Angkatan</th>
+                  <th>IPK</th>
+                  <th>Predikat</th>
+              </tr>
+          </thead>
+          <tbody>
+              <?php
+                // buat array
+                echo "<hr>";
+                $ar_mahasiswa = [$mhs1,$mhs2,$mhs3,$mhs4];
+                foreach($ar_mahasiswa as $mhs):
+              ?>
+              <tr>
+                <td><?php echo $mhs->no; ?></td>
+                <td><?php echo $mhs->nim; ?></td>
+                <td><?php echo $mhs->nama; ?></td>
+                <td><?php echo $mhs->prodi; ?></td>
+                <td><?php echo $mhs->thn_angkatan; ?></td>
+                <td><?php echo $mhs->ipk; ?></td>
+                <td><?php echo $mhs->predikat_ipk(); ?></td>
+
+              </tr>
+            <?php endforeach; ?>
+          </tbody>
+      </table>
+
+      <script type="text/javascript">
+      $(document).ready( function () {
+          $('#table_id').DataTable();
+      } );
+      </script>
+
 </body>
 </html>
