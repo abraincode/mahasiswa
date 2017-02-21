8e8a1049d5e55eb109af1be2baa9efad7b0ea250
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-21 12:21:35 +0700

    tambah fungsi predikat ipk

--- class_mahasiswa.php
+++ class_mahasiswa.php
@@ -16,6 +16,17 @@ class Mahasiswa {
     $this->nama = $nama;
   }
 
+  function predikat_ipk()
+  {
+    if ($this->ipk < 2.0 ) return "Cukup";
+    elseif ($this->ipk >= 2.0 && $this->ipk < 3.0)
+      return "Baik";
+    elseif ($this->ipk >= 3.0 && $this->ipk < 3.75)
+      return "Memuaskan";
+    elseif ($this->ipk >= 3.75 && $this->ipk <= 4)
+      return "Cum Laude";
+  }
+
 }
 
 ?>
