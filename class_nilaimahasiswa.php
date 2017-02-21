296cd3b07daa08e5de5d51151989f6581568a4c0
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-22 03:00:07 +0700

    update function grade

--- class_nilaimahasiswa.php
+++ class_nilaimahasiswa.php
@@ -12,12 +12,22 @@ class NilaiMahasiswa {
     $this->nim = $nim;
   }
 
-  function grade(){
-
+  function grade()
+  {
+    if ($this->nilai < 35 ) return "E";
+    elseif ($this->nilai >= 36 && $this->nilai < 55)
+      return "D";
+    elseif ($this->nilai >= 56 && $this->nilai < 69)
+      return "C";
+    elseif ($this->nilai >= 70 && $this->nilai <= 84)
+      return "B";
+    elseif ($this->nilai >= 85 && $this->nilai <= 100)
+      return "A";
   }
 
-  function hasil(){
-    
+  function hasil()
+  {
+
   }
 }
  ?>
