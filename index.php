abaf5ccfc0b180f0d4956dbbf4ff68a12a3bbe0e
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-26 20:23:13 +0700

    update index

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Data Mahasiswa</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="cover.css">
+    <script type="text/javascript" src="jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">Ganteng</h3>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="/">Home</a></li>
+                  <li><a href="daftar_mahasiswa.php">Daftar Mahasiswa</a></li>
+                  <li><a href="nilai_mahasiswa.php">Nilai Mahasiswa</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">Dhimas Ganteng</h1>
+            <p class="lead">Katakan Dhimas Ganteng Banget Katakan Dhimas Ganteng Banget Katakan Dhimas Ganteng Banget Katakan Dhimas Ganteng Banget Katakan Dhimas Ganteng Banget.</p>
+            <p class="lead">
+              <a href="facebook.com/dhimasanb" class="btn btn-lg btn-default">Yuk Kenalan sama Dhimas Ganteng</a>
+            </p>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </body>
+</html>
